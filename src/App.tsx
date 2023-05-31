b527ec3a1b7f7cdf93776fba4213c8a8ec6d1056
Isabel <isamorenoc08@gmail.com>
2023-05-31 14:03:48 -0500

    lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { AppRouter } from './Router';
 import { BrowserRouter } from 'react-router-dom';
 import { NotificationProvider } from './context/notification.context';
+import { Suspense } from 'react'
 
 function App() {
   return (
     <NotificationProvider>
     <BrowserRouter>
+    <Suspense fallback={'cargando...'}>
     <AppRouter />
+    </Suspense>
     </BrowserRouter>
     </NotificationProvider>
   );
--- src/pages/character/index.tsx
+++ src/pages/character/index.tsx
@@ -1,10 +1,18 @@
-import { useParams } from "react-router-dom";
-import { characters } from "../../api/characters";
-import React from "react";
-import { ICharacter } from "./interfaces/character.interface";
-import { Box, Chip, CircularProgress, Container, Divider, Grid, Typography } from "@mui/material";
+import { useParams } from 'react-router-dom';
+import { characters } from '../../api/characters';
+import React from 'react';
+import { ICharacter } from './interfaces/character.interface';
+import {
+  Box,
+  Chip,
+  CircularProgress,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
 
-export const CharacterPage: React.FC = () => {
+const CharacterPage: React.FC = () => {
   const { id } = useParams();
 
   const [loading, setLoading] = React.useState<boolean>(true);
@@ -20,24 +28,32 @@ export const CharacterPage: React.FC = () => {
   }, [id]);
 
   return (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: '100%' }}>
       <Container maxWidth="xl">
         {loading ? (
-          <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
-          <CircularProgress />
-        </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+            <CircularProgress />
+          </Box>
         ) : (
-          <Grid sx={{mt:2}} container columnSpacing={2}>
+          <Grid sx={{ mt: 2 }} container columnSpacing={2}>
             <Grid item xs={6}>
-                <Typography variant='h1'>{character!.name}</Typography>
-                <Divider/>
-                <Typography variant="h6">{character!.origin.name}</Typography>
-                <Box sx={{mt: 2}}>
-                    <Chip color="primary" variant="outlined" label={character!.status} />
-                </Box>
+              <Typography variant="h1">{character!.name}</Typography>
+              <Divider />
+              <Typography variant="h6">{character!.origin.name}</Typography>
+              <Box sx={{ mt: 2 }}>
+                <Chip
+                  color="primary"
+                  variant="outlined"
+                  label={character!.status}
+                />
+              </Box>
             </Grid>
-            <Grid item xs={6}> 
-              <img alt='img' src={character!.image} style={{width:"100%", borderRadius:"0.5em"}} />
+            <Grid item xs={6}>
+              <img
+                alt="img"
+                src={character!.image}
+                style={{ width: '100%', borderRadius: '0.5em' }}
+              />
             </Grid>
           </Grid>
         )}
@@ -45,3 +61,5 @@ export const CharacterPage: React.FC = () => {
     </Box>
   );
 };
+
+export default CharacterPage;
