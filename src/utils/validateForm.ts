8f2d1ace9c88d88ded34aa2c034d69561fca2255
Isabel <isamorenoc08@gmail.com>
2023-05-31 13:42:31 -0500

    login mejorado

--- src/utils/validateForm.ts
+++ src/utils/validateForm.ts
@@ -1,6 +1,11 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 export const LoginValidate = yup.object().shape({
-    username: yup.string().trim().required("El username es requerido"),
-    password: yup.string().trim().required("El password es requerido"),
+    username: yup.string().trim().required('El username es requerido'),
+  password: yup
+    .string()
+    .trim()
+    .required('El password es requerido')
+    .min(4, 'El mínimo debe ser 4 caracteres')
+    .max(10, 'El máximo debe ser 10 caracteres'),
 })
